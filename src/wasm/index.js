5e25c6bd52f24f8cc1b126b86194406056f5ceca
Paul Wilson <paul.wilson@ktech.com>
2019-11-27 15:44:29 +0000

    WIP

--- src/wasm/index.js
+++
@@ -1,8 +0,0 @@
-import {BASE} from '../config';
-import module from './crate/Cargo.toml';
-
-export const start = (onprogress, end) => {
-	const result = module.run(BASE, onprogress);
-	onprogress(result);
-	end();
-}
